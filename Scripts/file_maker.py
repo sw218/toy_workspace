299c8caecdbc270e280b05f0ed3b97c02b3d5b00
DESKTOP-M3CR7II\kojeomstudio <kojeomstudio@gmail.com>
2024-11-02 17:12:17 +0900

    # 샘플 코드.

---
+++ Scripts/file_maker.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+
+# 현재 시간을 파일 이름 형식에 맞게 가져오기 ('YYYYMMDD_HHMMSS')
+current_time_prefix = datetime.now().strftime('%Y%m%d_%H%M%S')
+
+dest_folder_path = "C:\\dist_folder"
+
+# 파일 이름 생성 (현재 시간 + '_log.txt')
+file_name = f'{dest_folder_path}\\{current_time_prefix}_log.txt'
+
+# 파일 생성 및 실행 시간 기록
+with open(file_name, 'w', encoding='utf-8') as file:
+    file.write(f'프로그램 실행 시간: {current_time_prefix}\n')
+
+print(f"{file_name} 파일이 생성되었습니다.")
