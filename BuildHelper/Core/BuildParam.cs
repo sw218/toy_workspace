11813e43fbb41fa32cde4ceffa88fcc5e631d368
kojeomstudio <kojeomstudio@gmail.com>
2024-11-30 18:24:40 +0900

    # 샘플용 빌드 헬퍼 코드 작업 중. / gitignore 파일 수정.

---
+++ BuildHelper/Core/BuildParam.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuildHelper.Core
+{
+    internal class BuildParam
+    {
+    }
+}
---
+++ BuildHelper/Core/Logger.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuildHelper.Core
+{
+    public enum ELogType
+    {
+        INFO,
+        WARNING,
+        ERROR,
+    }
+
+    internal class Logger
+    {
+        public static readonly Logger Instance = new Logger();
+
+        public void Log(string InMessage, ELogType InType = ELogType.INFO)
+        {
+            switch (InType)
+            {
+                case ELogType.INFO:
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    break;
+                case ELogType.WARNING:
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    break;
+                case ELogType.ERROR:
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    break;
+            }
+            // 타입에만 색상 적용.
+            Console.Write($"[{InType.ToString()}]");
+            Console.ResetColor();
+
+            Console.WriteLine($"::[{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")}]::[message : {InMessage}]");
+        }
+    }
+}
