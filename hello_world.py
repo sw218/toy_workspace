0249269dbd0e4d6df43811d2364eee6f0d69c516
swkim <59859634+sw218@users.noreply.github.com>
2025-05-07 10:11:36 +0900

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,5 +1,5 @@
 
 my_name_is = "kuma"
-print(f"my name is : {my_name_is} ~ hello world!!")
+#print(f"my name is : {my_name_is} ~ hello world!!")
 
-print(f"hello my teamcity world!")
+#print(f"hello my teamcity world!")
