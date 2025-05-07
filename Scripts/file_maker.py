7dac58887802449d6c5131dd92abcfbc9767b88c
swkim <59859634+sw218@users.noreply.github.com>
2025-05-07 17:00:22 +0900

    Update file_maker.py

--- Scripts/file_maker.py
+++ Scripts/file_maker.py
@@ -3,10 +3,10 @@ from datetime import datetime
 # 현재 시간을 파일 이름 형식에 맞게 가져오기 ('YYYYMMDD_HHMMSS')
 current_time_prefix = datetime.now().strftime('%Y%m%d_%H%M%S')
 
-dest_folder_path = "/tmp"
+dest_folder_path = "/opt/buildagent/work"
 
 # 파일 이름 생성 (현재 시간 + '_log.txt')
-file_name = f'{dest_folder_path}\\{current_time_prefix}_log.txt'
+file_name = f'{dest_folder_path}/{current_time_prefix}_log.txt'
 
 # 파일 생성 및 실행 시간 기록
 with open(file_name, 'w', encoding='utf-8') as file:
